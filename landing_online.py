72c7c9de6aee799f0c18e2c7a69119e9a91f8ccb
660324 <59671452+660324@users.noreply.github.com>
2023-06-01 19:33:07 -0400

    Update landing_online.py

--- landing_online.py
+++ landing_online.py
@@ -8,13 +8,6 @@ st.set_page_config(layout="wide")  #use 'wide' mode and use column controls to m
 
 #streamlit run "C:/Users/yuhao1/Desktop/diversity/landing_online.py"
 
-st.write("username:", st.secrets["username"])
-st.write("password:", st.secrets["password"])
-st.write(
-    "Has environment variables been set:",
-    os.environ["username"] == st.secrets["password"],
-)
-
 st.title('Office of Institutional Research and Assessment Data Inventory')
 st.text("")
 st.text("")
